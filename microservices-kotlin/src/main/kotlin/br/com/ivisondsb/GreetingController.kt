c6cd13d658ba07357aceedc866ce2262097d7808
ivisondsb <ivisonk0@gmail.com>
2024-10-11 15:27:18 -0300

    First Steps in Kotlin with Spring Boot

---
+++ microservices-kotlin/src/main/kotlin/br/com/ivisondsb/GreetingController.kt
@@ -0,0 +1,17 @@
+package br.com.ivisondsb
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetingController {
+
+    val counter: AtomicLong = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String?): Greeting {
+        return Greeting(counter.incrementAndGet(), "Hello $name!")
+    }
+}
---
+++ microservices-kotlin/src/test/kotlin/br/com/ivisondsb/MainTests.kt
@@ -0,0 +1,13 @@
+package br.com.ivisondsb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
